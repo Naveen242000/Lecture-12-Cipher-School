533a624933af1b1f9fcde53620ade59179fbb4ea
Naveen242000 <111578221+Naveen242000@users.noreply.github.com>
2023-07-26 18:00:02 +0530

    order_of_access.cpp

---
+++ order_of_access.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+class A{
+public: 
+A(){
+cout<<"A's Const."<<endl;
+}
+~A(){
+cout<<"A's dest."<<endl;
+}
+};
+class B: public A{
+public:
+B(){
+cout<<B's const"<<endl;
+  }
+~B(){
+cout<<"B's dest"<<endl;
+}
+};
+int main(){
+  B obj;
+return 0;
+}
